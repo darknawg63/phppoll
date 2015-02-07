e804e195a976e878d4fba70d5d10e4b70c1f2910
darknawg63 <darknawg63@gmail.com>
2015-02-07 21:28:39 +0100

    Added poll listing functionality

--- index.php
+++ index.php
@@ -1,3 +1,19 @@
+<?php
+
+require_once 'app/init.php';
+
+$pollsQuery = $db->query("
+    SELECT id, question
+    FROM polls
+    WHERE DATE(NOW()) BETWEEN starts AND ends
+");
+
+while($row = $pollsQuery->fetchObject()) {
+    $polls[] = $row;
+}
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
     <head>
@@ -7,9 +23,14 @@
         <link rel="stylesheet" type="text/css" href="css/main.css">
     </head>
     <body>
-        <ul>
-            <li><a href="poll.php">Poll 1</a></li>
-            <li><a href="poll.php">Poll 2</a></li>
-        </ul>
+        <?php if(!empty($polls)): ?>
+            <ul>
+                <?php foreach ($polls as $poll): ?>
+                    <li><a href="poll.php?id=<?php echo $poll->id; ?>"><?php echo $poll->question; ?></a></li>
+                <?php endforeach; ?>
+            </ul>
+        <?php else: ?>
+            <p>Sorry, no polls available right now.</p>
+        <?php endif; ?>
     </body>
 </html>
