41bad27c6514c0bf7764fd9698b90af92e0d992a
darknawg63 <darknawg63@gmail.com>
2015-02-09 20:01:40 +0100

    Added PDO catch exception

--- app/init.php
+++ app/init.php
@@ -4,4 +4,10 @@ session_start();
 
 $_SESSION['user_id'] = 9;
 
-$db = new PDO('mysql:host=localhost;dbname=poll', 'root', 'password');
+try {
+    $db = new PDO('mysql:host=localhost;dbname=poll', 'root', 'password');
+}
+// to handle connection error
+catch(PDOException $exception){
+    echo "Connection error: " . $exception->getMessage();
+}
